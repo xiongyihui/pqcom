bf206c12ddb3b2ad361ab5f5dcb49ea5510151a4
Yihui Xiong <xiongyihui3@gmail.com>
2015-06-02 11:37:32 +0800

    use relative path

--- util.py
+++ util.py
@@ -8,8 +8,7 @@ VERSION = 0.3
 TRANS_STRING = ''.join(chr(i) for i in range(0, 0x20) + range(0x80, 0x100))
 TRANS_TABLE = string.maketrans(TRANS_STRING, ''.ljust(len(TRANS_STRING), '.'))
 
-
-script_path = os.path.dirname(os.path.realpath(sys.argv[0]))
+script_path = os.path.dirname(sys.argv[0])
 
 def resource_path(relative_path):
     base_path = getattr(sys, '_MEIPASS', script_path)
